147d52f2cbaa13f7bb1efd39883c3fcab1adf6db
Truong Nguyen <nguyenxuantruong@tma.com.vn>
2019-10-04 16:24:17 +0700

    init practice

---
+++ managedpractice/src/main/java/tma/thiendang/managedpractice/management/ManagedPracticeApplication.java
@@ -0,0 +1,13 @@
+package tma.thiendang.managedpractice.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagedPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagedPracticeApplication.class, args);
+	}
+
+}
